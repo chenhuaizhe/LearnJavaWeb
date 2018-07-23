0b79d9ff2a21245c436a06dd4359809a17c642ef
Nine <chenyuan@iwown.com>
2018-07-23 19:36:38 +0800

    init

---
+++ demo2/src/main/java/com/example/demo/test/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public String test(){
+		return "this is api NO.0";
+	}
+}
